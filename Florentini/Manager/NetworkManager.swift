d85e68c6360009d30bfe4f455c1ebe7c80592f6e
Matsota Andrew <matsotaandrew@icloud.com>
2020-02-26 00:10:49 +0200

    update

--- Florentini/Manager/NetworkManager.swift
+++ Florentini/Manager/NetworkManager.swift
@@ -27,7 +27,8 @@ class NetworkManager {
             }
         }
     }
-    
+ 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //MARK: Получение всех сообщений  для чата сотрудников
     func workersMessagesLoad(success: @escaping([DatabaseManager.ChatMessages]) -> Void, failure: @escaping(Error) -> Void) {
         if AuthenticationManager.shared.currentUser?.uid == nil {
@@ -42,6 +43,23 @@ class NetworkManager {
             })
         }
     }
+//  Это для чата.
+//  Set в Firebase работает отлично. Сетит мне данные о сообщении по шаблону, который находится в database. А сам метод пока что находится в WorkersChatViewController на 135 позиции.( //MARK: Метод-Алерт для сообщения )
+//  На breakpoint'е показывает, что информации нет, когда я ее пытаюсь достать из firebase. Хотя метод вроде верный. По-крайне мере не вижу ошибки. Делал как и раньше
+//  Когда в DatabaseManager я вместе " guard let name = dictionary[DatabaseManager.ChatMessagesCases.name.rawValue] as? String ... else {return nil} " ставлю " let name = dictionary[DatabaseManager.ChatMessagesCases.name.rawValue] as! String ... " , то мне выдает Fatal Error, но я успеваю увидеть в консоли, что кусочек информации о сообщени вытянулся.
+//  DatabaseManager.ChatMessages сделал по аналогии, как и другие. Все String у меня через специальный enum для соответствующих нужд, что не ошибиться, когда их печают (как в прошлый раз).
+//  Но я раньше не делал init шаблона с Date, но timeStamp в firebase НЕ nil, и помеченно как "временная метка", то есть как Date
+//  Пример: DatabaseManager.ChatMessagesCases.name.rawValue
+//  "workersMessages" написан правильно, как и в firebase.
+    
+//MARK: СПОСОБ ВХОДА В ЧАТ:
+/// run app ->  menu ->  "FEEDBACK" -> Опустить вниз до "Отзыв":
+    /// строка "Введите имя":
+    // /WorkSpace
+    /// строка "Введите отзыв":
+    // Go/
+    /// -> login: test@test.com  pass: 123456  ->  Вверху справа Чат.
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
 
 extension NetworkManager {
